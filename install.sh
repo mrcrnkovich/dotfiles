9a70aa4b55d0f7a3734b27a0a43eef8290f27da2
mrcrnkovich <mrcrnkovich@gmail.com>
2021-03-31 21:33:14 -0400

    install updates

--- install.sh
+++ install.sh
@@ -14,12 +14,12 @@ function link {
   ln -sf ${dotfilesDir}/${2} ${dest}
 }
 
-function bin {
+function linkBin {
     mkdir -p $HOME/bin
     
     for file in $(ls "$dotfilesDir/bin/"); do
       echo "Linking /Bin: $file"  
-      ln -sf "$dotfilesDir/bin/$file" "$HOME/bin/${file%.*}"
+      ln -sf "$dotfilesDir/bin/$file" "${HOME}/bin/${file%.*}"
     done
 }
 
@@ -27,17 +27,17 @@ function bin {
 #                     destination dotfile_location                     #
 #----------------------------------------------------------------------#
 
-link '.vimrc' 'nvim/init.vim'
-link '.zshrc' 'shell/zshrc'
-link '.xinitrc' '/X11/xinitrc'
-link '.Xresources' '/X11/Xresources'
-link '.config/i3/config' 'i3/config'
-link '.config/i3status/config' 'i3/i3status/config'
-link '.config/picom.conf' 'config/picom.conf'
-link '.config/rofi/config.rasi' 'config/rofi/config.rasi'
+link   '.vimrc'                     'shell/vimrc'
+link   '.zshrc'                     'shell/zshrc'
+link   '.xinitrc'                   '/X11/xinitrc'
+link   '.Xresources'                '/X11/Xresources'
+link   '.config/i3/config'          'config/i3/config'
+link   '.config/i3status/config'    'config/i3status/config'
+link   '.config/picom.conf'         'config/picom.conf'
+link   '.config/rofi/config.rasi'   'config/rofi/config.rasi'
 
 #----------------------------------------------------------------------#
 #                       link all user bin files                        #
 #----------------------------------------------------------------------#
 
-bin
+linkBin
