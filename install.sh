0132637fc844f3d2f1539c5056d69761fef34ec4
Mike <mrcrnkovich@gmail.com>
2021-09-27 08:18:42 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,19 +8,19 @@ cd $HOME
 #----------------------------------------------------------------------#
 
 function link {
-  dest="${HOME}/${2}"
+    dest="${HOME}/${2}"
 
-  echo "Creating new symlink: ${dest}"
-  # need to mkdir for missing dirs
-  ln -sf ${dotfilesDir}/${1} ${dest}
+    echo "Creating new symlink: ${dest}"
+    # need to mkdir for missing dirs
+    ln -sf ${dotfilesDir}/${1} ${dest}
 }
 
 function linkBin {
     mkdir -p $HOME/bin
     
     for file in $(ls "$dotfilesDir/bin/"); do
-      echo "Linking /Bin: $file"  
-      ln -sf "$dotfilesDir/bin/$file" "${HOME}/bin/${file%.*}"
+        echo "Linking /Bin: $file"  
+        ln -sf "$dotfilesDir/bin/$file" "${HOME}/bin/${file%.*}"
     done
 }
 
@@ -30,14 +30,17 @@ function linkBin {
 
 link 'home/vimrc'                '.vimrc'
 link 'home/zshrc'                '.zshrc'
-link 'home/xinitrc'              '.xinitrc'
-link 'home/Xresources'           '.Xresources'
-link 'home/background.jpg'       '.background.jpg'
-link 'home/fehbg'                '.fehbg'
-link 'config/i3/config'          '.config/i3/config'
-link 'config/i3status/config'    '.config/i3status/config'
-link 'config/picom.conf'         '.config/picom.conf'
-link 'config/rofi/config.rasi'   '.config/rofi/config.rasi'
+
+if [[ $OSTYPE == 'linux-gnu' ]] then
+    link 'home/xinitrc'              '.xinitrc'
+    link 'home/Xresources'           '.Xresources'
+    link 'home/background.jpg'       '.background.jpg'
+    link 'home/fehbg'                '.fehbg'
+    link 'config/i3/config'          '.config/i3/config'
+    link 'config/i3status/config'    '.config/i3status/config'
+    link 'config/picom.conf'         '.config/picom.conf'
+    link 'config/rofi/config.rasi'   '.config/rofi/config.rasi'
+fi
 
 #----------------------------------------------------------------------#
 #                       link all user bin files                        #
