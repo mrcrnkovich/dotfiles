134e364c8fffda072ab39965ce82d1cfcfdfc153
Mike Crnkovich <mrcrnkovich@gmail.com>
2021-12-04 10:50:15 -0500

    updates to background path and path variable

--- arch_load.sh
+++ arch_load.sh
@@ -10,12 +10,18 @@ mount /dev/sda3 /mnt
 mkdir /mnt/boot
 mount /dev/sda1 /mnt/boot
 
-pacstrap /mnt base base-devel linux linux-firmware vim openssh grub
+pacstrap /mnt base base-devel linux linux-firmware vim openssh iwctl 
+
+arch-chroot /mnt
 
 locale-gen
 echo "LANG=en_US.UTF_8"   >    /etc/locale.conf
 
+reboot
+
+
 # move this to the install script
-pacman -S i3-gaps zsh ranger feh
+pacman -Syu
+pacman -S i3-gaps zsh ranger feh bluetoothctl xorg xorg-utils
 git clone https://github.com/mrcrnkovich/dotfiles.git
 cd dotfiles && ./install.sh
