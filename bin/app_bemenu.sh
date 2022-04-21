f4b4b3d6b462ade066717c9716121bb13f95e61c
Mike Crnkovich <mrcrnkovich@gmail.com>
2022-04-20 22:16:36 -0400

    Moving to Sway full-time

---
+++ bin/app_bemenu.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+
+bemenu-run --fn 16 --list=10\
+           --nf "#282828" --nb="#ebdbb2F0"\
+           --tb "#ebdbb2" --tf "#282828"\
+           --hb "#282828" --hf "#98971a"\
+           --ff "#282828" --fb "#ebdbb2"\
+           --prompt " >_ "\
+           --width-factor 0.8\
+           --no-overlap
